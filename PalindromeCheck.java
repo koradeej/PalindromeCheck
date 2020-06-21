b41455106bf1f4af3cf3f228ec77f798d962ae83
koradeej <67166601+koradeej@users.noreply.github.com>
2020-06-21 11:47:42 -0400

    Add files via upload

---
+++ PalindromeCheck.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class PalindromeCheck{
+	public static void main(String[] args){
+		
+		System.out.print("Enter 0 if you want to input your own 4-digit number. Enter 1 if you want it randomly generated");
+		Scanner input = new Scanner (System.in);
+		int answer = input.nextInt();
+		
+	}
+}
